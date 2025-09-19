78a34a2a4405a6add4884614598b041d89d75eca
Giovanni Humberto Tamborim <157848122+tamborim13@users.noreply.github.com>
2025-09-19 19:18:01 -0300

    Commit css

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }))
 app.use("/uploads", express.static("uploads"));
 app.use("/", produtoRoutes);
+app.use(express.static("public"))
 
 
 sequelize.sync().then(() => {
